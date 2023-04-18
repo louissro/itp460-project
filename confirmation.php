61ac180dbaf46b2623893c3f4835ef425f6a9e7f
Ashley <41446708+ashleysol@users.noreply.github.com>
2023-04-17 17:22:01 -0700

    email test code

--- confirmation.php
+++ confirmation.php
@@ -1,3 +1,43 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$phpmailer = new PHPMailer();
+
+try {
+    //Server settings
+    $phpmailer->isSMTP();
+    $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
+    $phpmailer->SMTPAuth = true;
+    $phpmailer->Port = 2525;
+    $phpmailer->Username = 'b0b97d38f44b87';
+    $phpmailer->Password = '4955b6f7008913';
+
+
+    //Recipients
+    $phpmailer->setFrom('from@example.com', 'Mailer');
+    $phpmailer->addAddress('ashleysol2001@gmail.com', 'Joe User'); 
+
+    //Content
+    $phpmailer->isHTML(true);                                  //Set email format to HTML
+    $phpmailer->Subject = 'Here is the subject';
+    $phpmailer->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $phpmailer->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    //Commenting this out for now to prevent email spam
+    // $phpmailer->send();
+    // echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$phpmailer->ErrorInfo}";
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
